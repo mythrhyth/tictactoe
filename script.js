a294891a389844cc3a27baf4524611746f72ce91
Rhythm Bhetwal <100419159+mythrhyth@users.noreply.github.com>
2022-11-21 18:37:32 +0545

    Update script.js

--- script.js
+++ script.js
@@ -13,14 +13,14 @@ const changeTurn = ()=>{
 const checkWin = ()=>{
     let boxtext = document.getElementsByClassName('boxtext')
     let wins = [
-        [0, 1, 2, 5, 5, 0],
-        [3, 4, 5, 5, 15, 0],
-        [6, 7, 8, 5, 25, 0],
-        [0, 3, 6, -5, 15, 90],
-        [1, 4, 7, 5, 15, 90],
-        [2, 5, 8, 15, 25, 90],
-        [0, 4, 8, 5, 15, 45],
-        [2, 4, 6, 5, 15, 135]
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
     ];
     wins.forEach(e =>{
         if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "")){
@@ -28,7 +28,6 @@ const checkWin = ()=>{
             Gameover = true;
             gameover.play();
             document.getElementsByTagName('img')[0].style.width = "205px";
-            document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`;
         };
     });
 }[
